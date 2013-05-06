d2c66af8322bb62c6aa5a69aed32bf9242787b93
David <david@gul.es>
2013-05-06 20:17:44 +0200

    Added simple status script

---
+++ tareas/status-infrastructure.sh
@@ -0,0 +1,42 @@
+#!/bin/bash 
+
+. config.sh
+
+pushd $ZOE_BASE >/dev/null
+echo -n "" > $ZOE_PIDS
+
+RUNNING=""
+STOPPED=""
+
+function check() {
+    NAME=$1
+    SCRIPT=$2
+    WORKING=`ps ax | grep $SCRIPT | grep -v grep | wc -l`
+    if [ "$WORKING" -eq "1" ]
+    then 
+        RUNNING="$RUNNING $NAME"
+    else
+        STOPPED="$STOPPED $NAME"
+    fi
+}
+
+check server server.py
+
+for AGENT in $ENABLED_AGENTS
+do
+    check $AGENT ${AGENT}_agent.py
+done
+
+echo "Running:"
+for AGENT in $RUNNING
+do
+    echo "    $AGENT"
+done
+
+echo
+echo "Stopped:"
+for AGENT in $STOPPED
+do
+    echo "    $AGENT"
+done
+
