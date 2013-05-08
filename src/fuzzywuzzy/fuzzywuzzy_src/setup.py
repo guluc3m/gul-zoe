2644f1c97ac8575334c87ecac5d9b58dc2e7f23c
voiser <david@gul.es>
2013-05-08 12:33:48 +0200

    Cleaning third party source, leaving their URLs

--- src/fuzzywuzzy/fuzzywuzzy_src/setup.py
+++
@@ -1,9 +0,0 @@
-from distutils.core import setup
-
-setup(name='fuzzywuzzy',
-    version='0.1',
-    description='Fuzzy string matching in python',
-    author='Adam Cohen',
-    author_email='adam@seatgeek.com',
-    url='https://github.com/seatgeek/fuzzywuzzy/',
-    packages=['fuzzywuzzy'])
