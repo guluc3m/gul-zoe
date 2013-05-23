7ff1bee294a73cf3b8a09690fd7e72a3103429da
David <david@gul.es>
2013-05-23 20:25:50 +0200

    Added a script to set the required configuration variables

---
+++ zoe.sh
@@ -0,0 +1,31 @@
+
+# The project path
+export ZOE_HOME=/path/to/gul-zoe
+
+# If you have all the required libraries installed, you will not need this.
+# If you don't, clone gul-zoe-dependencies projects and set this variable:
+#export PYTHONPATH=/path/to/gul-zoe-dependencies
+
+# Typical values
+export ZOE_SERVER_HOST=localhost
+export ZOE_SERVER_PORT=30000
+export PYTHON3=python3.3
+
+# Get this from twitter
+export zoe_twitter_consumer_key="..."
+export zoe_twitter_consumer_secret="..."
+export zoe_twitter_access_token="..."
+export zoe_twitter_access_token_secret="..."
+
+# Paramters for Google Talk
+export zoe_jabber_host="talk.google.com"
+export zoe_jabber_port="5222"
+export zoe_jabber_user="..."
+export zoe_jabber_password=""
+
+# Parameters for a GMail account
+export zoe_mail_smtp="smtp.gmail.com"
+export zoe_mail_port="587"
+export zoe_mail_user="$zoe_jabber_user"
+export zoe_mail_password="$zoe_jabber_password"
+
