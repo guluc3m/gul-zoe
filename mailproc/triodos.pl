9c2992494cd4aa72c379502f6e717e8dde952a16
David <david@gul.es>
2013-07-06 20:48:15 +0200

    I was so ashamed of the triodos scriipt

--- mailproc/triodos.pl
+++ mailproc/triodos.pl
@@ -1,36 +1,27 @@
 #!/usr/bin/env perl
 
+use Encode;
+use MIME::QuotedPrint::Perl;
+
 $file = $ARGV[0];
 
 local $/;
-open(FILE, $file);
+open(FILE, "<:encoding(iso-8859-1)", $file);
 $document = <FILE>; 
 close (FILE);
-$document =~ s/=\n//g;
-$document =~ s/\n>*//g;
-$document =~ s/\n/ /g;
+
+$document = decode_qp($document);
+#$document =~ s/\n/ /g;
 
 $incoming = $document =~ /alerta de Abono Superior/i;
 exit unless $incoming;
 
-# Remove almost everything, leaving only the important part of the message
-# I'm pretty sure a Perl Jedi can do this with just one command.
-$gist = $document;
-$gist =~ s/.*?estimado cliente://i;
-$gist =~ s/Quedamos a su disposic.*//i;
+my ($d, $m, $y, $account, $amount, $balance) = 
+	$document =~ m/.*Atendiendo a su petici.n, e-Triodos le informa que el d.a (\d\d)-(\d\d)-(\d\d\d\d) se ha producido un abono en su cuenta (.*?) por importe de (.*?) EUR. El saldo actual es de (.*?) EUR.*/;
 
-# find the relevant parameters of the message
-$gist =~ /(\d\d)-(\d\d)-(\d\d\d\d)/;
-$d = $1;
-$m = $2;
-$y = $3;
-
-$gist =~ /cuenta (.*?) por importe/;
-$account = $1;
-
-$gist =~ /por importe de (.*?) EUR./;
-$amount = $1;
 $amount =~ s/,/\./g;
+$balance =~ s/,/\./g;
+$what = "Abono superior";
 
 # Don't forget the \n
-print "dst=banking&tag=entry&date=$y-$m-$d&account=$account&amount=$amount&what=Intereses\n"
+print "dst=banking&tag=entry&date=$y-$m-$d&account=$account&amount=$amount&what=$what\n"
