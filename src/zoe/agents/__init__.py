7c070f9909baf344405e58dcd3bb99ff09e59e33
David <david@gul.es>
2013-08-11 13:49:25 +0200

    removing twitter python agent

--- src/zoe/agents/__init__.py
+++ src/zoe/agents/__init__.py
@@ -29,7 +29,6 @@ from zoe.agents.echo import *
 from zoe.agents.users import *
 from zoe.agents.activities import *
 from zoe.agents.broadcast import *
-from zoe.agents.twitter import *
 from zoe.agents.banking import *
 from zoe.agents.inventory import *
 from zoe.agents.stalker import *
